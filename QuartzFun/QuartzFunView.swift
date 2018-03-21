d34dc5dbe083212927f2f89a5ee7c947d84dcbf0
TheLostKing <calderonezj@gmail.com>
2018-03-21 14:23:10 -0500

    Final

--- QuartzFun/QuartzFunView.swift
+++ QuartzFun/QuartzFunView.swift
@@ -32,10 +32,11 @@ class QuartzFunView: UIView {
     var currentColor = UIColor.red
     var useRandomColor = false
     
-    // Internal properties
-    private let image = UIImage( named:"iphone")
+    // Internal properties private let image = UIImage( named:" iphone")
+    private let image = UIImage( named:"image")
     private var firstTouchLocation = CGPoint.zero
     private var lastTouchLocation = CGPoint.zero
+    private var redrawRect = CGRect.zero
 
     /*
     // Only override draw() if you perform custom drawing.
@@ -52,21 +53,36 @@ class QuartzFunView: UIView {
             }
             firstTouchLocation = touch.location( in: self)
             lastTouchLocation = firstTouchLocation
+            redrawRect = CGRect.zero
             setNeedsDisplay()
         }
     }
-    
     override func touchesMoved(_ touches: Set < UITouch >, with event: UIEvent?) {
         if let touch = touches.first {
             lastTouchLocation = touch.location( in: self)
-            setNeedsDisplay()
+            if shape == .image {
+                let horizontalOffset = image!.size.width / 2
+                let verticalOffset = image!.size.height / 2
+                redrawRect = redrawRect.union( CGRect( x: lastTouchLocation.x - horizontalOffset, y: lastTouchLocation.y - verticalOffset, width: image!.size.width, height: image!.size.height))
+            }
+            else {
+                redrawRect = redrawRect.union(currentRect())
+            }
+            setNeedsDisplay( redrawRect)
         }
     }
-    
     override func touchesEnded(_ touches: Set < UITouch >, with event: UIEvent?) {
         if let touch = touches.first {
             lastTouchLocation = touch.location( in: self)
-            setNeedsDisplay()
+            if shape == .image {
+                let horizontalOffset = image!.size.width / 2
+                let verticalOffset = image!.size.height / 2
+                redrawRect = redrawRect.union( CGRect( x: lastTouchLocation.x - horizontalOffset, y: lastTouchLocation.y - verticalOffset, width: image!.size.width, height: image!.size.height))
+            }
+            else {
+                redrawRect = redrawRect.union(currentRect())
+            }
+            setNeedsDisplay( redrawRect)
         }
     }
 
@@ -75,26 +91,27 @@ class QuartzFunView: UIView {
         context?.setLineWidth( 2.0)
         context?.setStrokeColor( currentColor.cgColor)
         context?.setFillColor( currentColor.cgColor)
-        let currentRect = CGRect( x: firstTouchLocation.x, y: firstTouchLocation.y, width: lastTouchLocation.x - firstTouchLocation.x, height: lastTouchLocation.y - firstTouchLocation.y)
         switch shape {
         case .line:
             context?.move( to: CGPoint( x: firstTouchLocation.x, y: firstTouchLocation.y))
             context?.addLine( to: CGPoint( x: lastTouchLocation.x, y: lastTouchLocation.y))
-            context?.strokePath()
+                context?.strokePath()
         case .rect:
-            context?.addRect( currentRect)
+            context?.addRect( currentRect())
             context?.drawPath( using: .fillStroke)
         case .ellipse:
-            context?.addEllipse( in: currentRect)
+            context?.addEllipse( in: currentRect())
             context?.drawPath( using: .fillStroke)
         case .image:
             let horizontalOffset = image!.size.width / 2
             let verticalOffset = image!.size.height / 2
-            let drawPoint = CGPoint( x: lastTouchLocation.x - horizontalOffset, y: lastTouchLocation.y - verticalOffset)
-            image!.draw( at: drawPoint)
-            }
+            let drawPoint = CGPoint( x: lastTouchLocation.x - horizontalOffset, y: lastTouchLocation.y)
         }
+    }
     
+    func currentRect() -> CGRect {
+        return CGRect( x: firstTouchLocation.x, y: firstTouchLocation.y, width: lastTouchLocation.x - firstTouchLocation.x, height: lastTouchLocation.y - firstTouchLocation.y)
+    }
     
 
 }
